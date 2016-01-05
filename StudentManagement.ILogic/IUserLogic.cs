b19ad3a6a12312bbdc75d8084c09158697cca18a
Daniel <fengchenziyi@sina.com>
2016-01-05 12:29:59 +0800

    merge

---
+++ StudentManagement.ILogic/IUserLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StudentManagement.Data.Models;
+
+namespace StudentManagement.ILogic
+{
+    public interface IUserLogic
+    {
+        void Create(User model);
+        void Edit(User model);
+        void Delete(int id);
+        User Get(int id);
+        bool Login(string userName, string password);
+    }
+}
---
+++ StudentManagement.Logic/UserLogic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentManagement.Data.Models;
+using StudentManagement.ILogic;
+using StudentManagement.Infrustructure;
+using StudentManagement.IRepository;
+using StudentManagement.Repository;
+
+namespace StudentManagement.Logic
+{
+    public class UserLogic:IUserLogic
+    {
+        public UserLogic(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        private IUserRepository _userRepository = null;
+        public void Create(User model)
+        {
+            _userRepository=new UserRepository(null);
+        }
+
+        public void Edit(User model)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+
+        public User Get(int id)
+        {
+            User user = null;
+            return user;
+        }
+
+        public bool Login(string userName, string password)
+        {
+            bool result = false;
+            var user = _userRepository.Get(userName);
+            if (user!=null&&user.Password.Equals(Cryptography.Encrypt(password)))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
